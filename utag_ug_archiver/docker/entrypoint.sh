5d3b882d0ec39cc4e2eb54e00af38038a5573ade
oforiPrince <pofori034@st.ug.edu.gh>
2025-11-05 05:01:36 +0000

    update

--- utag_ug_archiver/docker/entrypoint.sh
+++ utag_ug_archiver/docker/entrypoint.sh
@@ -1,27 +1,27 @@
-# #!/bin/sh
-# set -e
+#!/bin/sh
+set -e
 
-# # If running as root (during some builds or manual runs), ensure directories are writable
-# if [ "$(id -u)" = "0" ]; then
-#     # chown /app and static/media so the unprivileged user can write
-#     if [ -d /app ]; then
-#         chown -R app:app /app || true
-#     fi
-#     if [ -d /app/staticfiles ]; then
-#         chown -R app:app /app/staticfiles || true
-#     fi
-#     if [ -d /app/media ]; then
-#         chown -R app:app /app/media || true
-#     fi
-# fi
+# If running as root (during some builds or manual runs), ensure directories are writable
+if [ "$(id -u)" = "0" ]; then
+    # chown /app and static/media so the unprivileged user can write
+    if [ -d /app ]; then
+        chown -R app:app /app || true
+    fi
+    if [ -d /app/staticfiles ]; then
+        chown -R app:app /app/staticfiles || true
+    fi
+    if [ -d /app/media ]; then
+        chown -R app:app /app/media || true
+    fi
+fi
 
-# # Run migrations and collectstatic. These will run as the current user (container will usually run as 'app').
-# if [ "${RUN_MIGRATIONS:-1}" = "1" ]; then
-#     python manage.py migrate --noinput
-# fi
+# Run migrations and collectstatic. These will run as the current user (container will usually run as 'app').
+if [ "${RUN_MIGRATIONS:-1}" = "1" ]; then
+    python manage.py migrate --noinput
+fi
 
-# if [ "${RUN_COLLECTSTATIC:-1}" = "1" ]; then
-#     python manage.py collectstatic --noinput
-# fi
+if [ "${RUN_COLLECTSTATIC:-1}" = "1" ]; then
+    python manage.py collectstatic --noinput
+fi
 
-# exec "$@"
+exec "$@"
