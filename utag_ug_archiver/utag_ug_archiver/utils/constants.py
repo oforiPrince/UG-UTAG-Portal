d61144327736dd29140854e636277d31f39fea4a
oforiPrince <pofori034@st.ug.edu.gh>
2026-01-12 06:45:34 +0000

    update

--- utag_ug_archiver/utag_ug_archiver/utils/constants.py
+++ utag_ug_archiver/utag_ug_archiver/utils/constants.py
@@ -1,2 +1,2 @@
 executive_members_position_order = ['President', 'Vice President', 'Secretary', 'Treasurer',"Women's Executive Officer"]
-executive_committee_members_position_order = ['President', 'Vice President', 'Secretary', 'Treasurer',"Women's Executive Officer",'Past President',"National President","CBAS Rep", "College of Humanities Rep", "College of Health Rep", "College of Education Rep"]
+executive_committee_members_position_order = ['President', 'Vice President', 'Secretary', 'Treasurer',"Women's Executive Officer",'Past President',"CBAS Rep", "College of Humanities Rep", "College of Health Rep", "College of Education Rep"]
