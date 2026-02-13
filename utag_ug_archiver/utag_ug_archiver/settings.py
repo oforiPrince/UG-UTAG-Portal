acfe2adecaf6a4f5d966d97146ab38436a76c358
oforiPrince <pofori034@st.ug.edu.gh>
2026-02-13 17:10:05 +0000

    update

--- utag_ug_archiver/utag_ug_archiver/settings.py
+++ utag_ug_archiver/utag_ug_archiver/settings.py
@@ -364,6 +364,7 @@ if not DEBUG:
 
 # Template caching for production
 if not DEBUG:
+    TEMPLATES[0]['APP_DIRS'] = False
     TEMPLATES[0]['OPTIONS']['loaders'] = [
         ('django.template.loaders.cached.Loader', [
             'django.template.loaders.filesystem.Loader',
